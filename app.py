94f3559ee7a5a3528f3ad7d3756f97ec3de2e0a3
oxuju <naijahuney@gmail.com>
2025-05-04 19:24:37 -0700

    made some changes to the app.py and the rundevsh. no matter what i did, i tried the url and everything and it wasn't working. but when i changed the flask_debug to one, i was able to copy and paste the http into the browser and get the results. im not sure if this was allowed, if not, i will find another way for it to work, but doing this was able for it to function and work.

--- app.py
+++ app.py
@@ -3,7 +3,21 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+#only using the app.py
+#no other libraries
 
-@app.route("/some_route_here", methods=["GET"])
-def method_name():
-    return {}
+#the route for the the app http://localhost:8080/api/calcs/8 (example)
+#so that would mean to make it /api/calcs/value(?)
+
+@app.route("/api/calcs/<value>", methods=["GET"])
+def calculate(value): #create a method name "calculate"
+    try:
+        num = int(value)
+        if num <= 0:
+            return "Input must be a positive integer", 404
+    except ValueError:
+        return "Input must be a valid integer", 400
+    return {
+        "dec": num - 1,
+        "hex": hex(num)
+    }
--- rundev.sh
+++ rundev.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 export FLASK_APP=app.py
-export FLASK_DEBUG=0
+export FLASK_DEBUG=1
 
 flask run --host=0.0.0.0 --port=8080
